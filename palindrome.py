5aaf6209f38eb3a288869571c501c0809b8cdc5b
groshevpavel <45458080+groshevpavel@users.noreply.github.com>
2019-09-20 14:29:53 +0300

    алгоритм проверки является ли строка палиндромом

---
+++ palindrome.py
@@ -0,0 +1,46 @@
+# https://ru.wikipedia.org/wiki/Палиндром
+#
+# алгоритм учитывает набор символов, которые НЕ принимают участия в решении что строка палиндром
+
+pol = ["Я иду с мечем судия", 'Sum summus mus ']
+pol += """Кони топот инок
+Но не речь, а черен он.
+Идем, молод, долом меди.
+Чин зван мечем навзничь.
+Голод, чем меч долог? """.splitlines()
+
+print(pol)
+
+skippers=[' ', "'", ",", ".", "\n", "?", "ь"]
+
+def is_pol(s:str, skippers=skippers)->float:
+    i, len_s = 0, len(s)
+    inc_left, inc_right = 0, 0
+
+    while i < len_s:
+        left, right = i + inc_left, (len_s - 1) - i - inc_right
+
+        left_s, right_s = s[left].lower(), s[right].lower()
+
+        while left_s in skippers:
+            inc_left += 1
+            left = i + inc_left
+            left_s = s[left].lower()
+
+        while right_s in skippers:
+            inc_right += 1
+            right = (len_s - 1) - i - inc_right
+            right_s = s[right].lower()
+
+
+        if left_s != right_s:
+            print(s[left].lower() , s[right].lower(), i, left, right, len_s)
+            return False
+        else:
+            i += 1
+
+        if left>=right:
+            return True
+
+
+print([is_pol(p) for p in pol])
